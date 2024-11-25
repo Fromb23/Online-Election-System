af0c04bf03c6f8ae7c02ac473bf6ced9015aa8fb
Francis Rombo <fromb@kali.>
2024-11-25 14:38:09 +0000

    more routes

--- backend/routes/CandidateRoutes.js
+++ backend/routes/CandidateRoutes.js
@@ -14,4 +14,60 @@ router.post('/', async (req, res) => {
     }
 });
 
+// Get candidates
+router.get('/', async (req, res) => {
+	try {
+		const candidates = await Candidate.findAll();
+		res.status(200).json(candidates);
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
+});
+
+// Get a candidate
+router.get('/:candidateId', async (req, res) => {
+	try {
+		const { candidateId } = req.params;
+		const candidate = Candidate.findByPk(candidateId);
+		if (!candidate) return res.status(401).json({ error: "Candidate not found" });
+		res.status(200).json(candidate);
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
+});
+
+// Update a candidate
+router.put('/:candidateId', async (req, res) => {
+	try {
+		const { candidateId } = req.params;
+		const candidate = await Candidate.findByPk(candidateId);
+		if (!candidate) return res.status(401).json({ error: "Candidate not found" });
+		const { candidateName, partyId, voted, voteCategoryId } = req.body;
+
+		if (candidateName !== undefined) candidate.candidateName = candidateName;
+		if (candidate.partyId !== undefined)  candidate.partyId = partyId;
+		if (candidate.voted !== undefined)  candidate.voted = voted;
+		if (candidate.voteCategoryId !== undefined) candidate.voteCategory = voteCategoryId;
+
+		candidate.save();
+		res.status(200).json(candidate);
+
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
+});
+
+// Delete a candidate
+router.delete('/:candidateId', async (req, res) => {
+	try {
+		const { candidateId } = req.params;
+		const candidate = await Candidate.findByPk(candidateId);
+		if (!candidate) return res.status(401).json({ error: "Candidate not found" });
+		candidate.destroy();
+		res.status(200).json({ "message": "Candidate deleted successfully" });
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
+});
+
 module.exports = router;
